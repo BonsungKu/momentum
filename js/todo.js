7a123520e11550966e9f236837efccdce6837972
BonsungKu <bonsung1114@icloud.com>
2021-09-12 17:09:02 +0900

    added files

--- js/todo.js
+++ js/todo.js
@@ -39,9 +39,10 @@ function handleToDoSubmit(event){
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
+
 const savedToDos = localStorage.getItem(TODOS_KEY);
-console.log(savedToDos);
 if(savedToDos !== null){
     const parsedTodos = JSON.parse(savedToDos);
-    parsedTodos.forEach();
+    console.log(parsedTodos);
+    parsedTodos.forEach((item) => console.log("this is the turn of", item));
 }
