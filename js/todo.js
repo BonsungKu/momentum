14380842a5e6c775f9d1de281aa81d71c58b66ef
BonsungKu <bonsung1114@icloud.com>
2021-09-14 23:27:47 +0900

    update some codes

--- js/todo.js
+++ js/todo.js
@@ -12,8 +12,9 @@ function saveToDos(){
 
 function deleteTodo(event){
     const li = event.target.parentElement;
-    console.log(li.id);
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 
@@ -54,3 +55,4 @@ if(savedToDos !== null){
     toDos = parsedTodos;
     parsedTodos.forEach(paintTodo);
 }
+
