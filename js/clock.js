2e5961ca782962c225875469cda4f43eabf34465
BonsungKu <bonsung1114@icloud.com>
2021-09-15 23:15:22 +0900

    add styles! part1

--- js/clock.js
+++ js/clock.js
@@ -5,8 +5,7 @@ function getClock(){
     //날짜 시간 호출
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
